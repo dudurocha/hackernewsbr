e9882a7a34e81a000a0be25beb21c21ce9b47b4e
fjsj <flaviojuvenal@gmail.com>
2013-03-26 18:27:03 -0300

    Using connect instead of Fiber

--- server/lib/headly/headly_server.js
+++ server/lib/headly/headly_server.js
@@ -1,40 +1,38 @@
 (function () {
-    if (!Meteor.headly) {	 
-	Meteor.headly = {};
+    if (!Meteor.headly) {        
+        Meteor.headly = {};
     }
     
     if (!Meteor.headly._options) {
-	Meteor.headly._options = {};
+        Meteor.headly._options = {};
     }
     
     Meteor.headly.config = function(options) {
-	Meteor.headly._options = options || Meteor.headly._options;
+        Meteor.headly._options = options || Meteor.headly._options;
     };
     
-    var Fiber = __meteor_bootstrap__.require('fibers');
+    var connect = __meteor_bootstrap__.require("connect");
     __meteor_bootstrap__.app
-	.use(function(req, res, next) {
-	    if (req.headers['user-agent'].indexOf('facebookexternalhit') !== -1) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		Fiber(function () {
-		    
-                    if (!Meteor.headly._options.tagsForRequest) {
-                        throw Error('headly was not configured with tagsForRequest function.');
-                    }
+    .use(connect.query())
+    .use(function(req, res, next) {
+        if (req.headers['user-agent'].indexOf('facebookexternalhit') !== -1) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+                
+            if (!Meteor.headly._options.tagsForRequest) {
+                throw Error('headly was not configured with tagsForRequest function.');
+            }
 
-		    var head = Meteor.headly._options.tagsForRequest(req);
-                    
-		    if (head) {
-			res.write('<head>');
-			res.write(head);
-			res.write('</head>');
-		    }
-		    res.end();
-
-		}).run();
-	    }
-	     else {
-		next();   
-	    }
-	});
+            var head = Meteor.headly._options.tagsForRequest(req);
+            
+            if (head) {
+                res.write('<head>');
+                res.write(head);
+                res.write('</head>');
+            }
+            res.end();
+        }
+         else {
+            next();   
+        }
+    });
 })();
