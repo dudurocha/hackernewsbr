3cca7a8168e8ac9cc747586bb87904fc44ebed36
fjsj <flaviojuvenal@gmail.com>
2013-03-26 17:21:34 -0300

    Setting open graph metatags for facebook requests

---
+++ server/head.js
@@ -0,0 +1,15 @@
+Meteor.headly.config({tagsForRequest: function(req) {
+  var urlSplitted = req.url.split('/');
+  var postId = urlSplitted[urlSplitted.length - 1];
+  var post = Posts.findOne(postId);
+  if (post) {
+    return '<meta property="og:title" content="' + post.headline + '" />' +
+           '<meta property="og:description" content="' + post.body + '" />' +
+           '<meta property="og:image" content="/public/img/brasil.png" />' +
+           '<meta property="og:site_name" content="Hacker News Brasil" />';
+  } else {
+    return '<meta property="og:title" content="Hacker News Brasil" />' +
+           '<meta property="og:image" content="/public/img/brasil.png" />' +
+           '<meta property="og:site_name" content="Hacker News Brasil" />';
+  }
+}});
---
+++ server/lib/headly/headly_server.js
@@ -0,0 +1,39 @@
+(function () {
+    if (!Meteor.headly) {	 
+	Meteor.headly = {};
+    }
+    
+    if (!Meteor.headly._options) {
+	Meteor.headly._options = {};
+    }
+    
+    Meteor.headly.config = function(options) {
+	Meteor.headly._options = options || Meteor.headly._options;
+    };
+    
+    __meteor_bootstrap__.app
+	.use(function(req, res, next) {
+	    if (req.headers['user-agent'].indexOf('facebookexternalhit') !== -1) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		Fiber(function () {
+		    
+                    if (!Meteor.headly._options.tagsForRequest) {
+                        throw Error('headly was not configured with tagsForRequest function.');
+                    }
+
+		    var head = Meteor.headly._options.tagsForRequest(req);
+                    
+		    if (head) {
+			res.write('<head>');
+			res.write(head);
+			res.write('</head>');
+		    }
+		    res.end();
+
+		}).run();
+	    }
+	     else {
+		next();   
+	    }
+	});
+})();
