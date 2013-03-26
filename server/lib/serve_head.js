526e3ba866a0e80d4eaf6bd5ca6b9b32e3c7198d
fjsj <flaviojuvenal@gmail.com>
2013-03-26 20:28:54 -0300

    Using fibers in serve_head

--- server/lib/serve_head.js
+++ server/lib/serve_head.js
@@ -1,10 +1,13 @@
 Meteor.serveHead = function (fn) {
+    var Fiber = __meteor_bootstrap__.require('fibers');
     var connect = __meteor_bootstrap__.require("connect");
     __meteor_bootstrap__.app
         .use(connect.query())
         .use(function(req, res, next) {
             if (req.headers['user-agent'].indexOf('facebookexternalhit') !== -1) {
-                res.end('<head>' + (fn(req) || '') + '</head>');
+                Fiber(function () {
+                    res.end('<head>' + (fn(req) || '') + '</head>');
+                }).run();
             } else {
                 next();   
             }
