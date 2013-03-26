eff840fb6a29da33bf4ca2dffad1c27a4293062f
fjsj <flaviojuvenal@gmail.com>
2013-03-26 18:33:50 -0300

    Aaaarrrghhh

--- server/lib/headly/headly_server.js
+++ server/lib/headly/headly_server.js
@@ -16,20 +16,12 @@
     .use(connect.query())
     .use(function(req, res, next) {
         if (req.headers['user-agent'].indexOf('facebookexternalhit') !== -1) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-                
             if (!Meteor.headly._options.tagsForRequest) {
                 throw Error('headly was not configured with tagsForRequest function.');
             }
 
             var head = Meteor.headly._options.tagsForRequest(req);
-            
-            if (head) {
-                res.write('<head>');
-                res.write(head);
-                res.write('</head>');
-            }
-            res.end();
+            res.end('<head>' + (head || '') + '</head>');
         }
          else {
             next();   
