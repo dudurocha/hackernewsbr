45d8fd072f41d1a0a3993ed2efbca7f85cd73ac8
fjsj <flaviojuvenal@gmail.com>
2013-03-26 18:53:36 -0300

    Absolute url in meta og:image

--- server/head.js
+++ server/head.js
@@ -9,11 +9,11 @@ Meteor.serveHead(function(req) {
   if (post) {
     return '<meta property="og:title" content="' + post.headline + '" />' +
            '<meta property="og:description" content="' + post.body + '" />' +
-           '<meta property="og:image" content="/public/img/brasil.png" />' +
+           '<meta property="og:image" content="' + Meteor.absoluteUrl() + 'public/img/brasil.png" />' +
            '<meta property="og:site_name" content="Hacker News Brasil" />';
   } else {
     return '<meta property="og:title" content="Hacker News Brasil" />' +
-           '<meta property="og:image" content="/public/img/brasil.png" />' +
+           '<meta property="og:image" content="' + Meteor.absoluteUrl() + 'public/img/brasil.png" />' +
            '<meta property="og:site_name" content="Hacker News Brasil" />';
   }
 });
