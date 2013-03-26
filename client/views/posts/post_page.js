4450d1e93e4313bdb04cabd7c80223a76af053a6
dudurocha <eduardo@traineeworld.com.br>
2013-03-26 16:52:47 -0300

    barra de title sem perda da barra do comentario

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -1,25 +1,33 @@
-Template.post_page.helpers({
-	post: function(){
-		var post = Posts.findOne(Session.get('selectedPostId'));
-		return post;
-	},
-	body_formatted: function(){
-		var converter = new Markdown.Converter();
-		var html_body=converter.makeHtml(this.body);
-		return html_body.autoLink();
-	},
-	canComment: function(){
-		return canComment(Meteor.user());
-	},
-	canView: function(){
-		return canView(Meteor.user());
-	}
-}); 
+(function () {
+  var post = null;
 
-Template.post_page.rendered = function(){
-	if((scrollToCommentId=Session.get('scrollToCommentId')) && !this.rendered && $('#'+scrollToCommentId).exists()){
-		scrollPageTo('#'+scrollToCommentId);
-		Session.set('scrollToCommentId', null);
-		this.rendered=true;
+	Template.post_page.created = function () {
+		post = Posts.findOne(Session.get('selectedPostId'));
 	}
-}
+
+	Template.post_page.helpers({
+		post: function(){
+			return post;
+		},
+		body_formatted: function(){
+			var converter = new Markdown.Converter();
+			var html_body=converter.makeHtml(this.body);
+			return html_body.autoLink();
+		},
+		canComment: function(){
+			return canComment(Meteor.user());
+		},
+		canView: function(){
+			return canView(Meteor.user());
+		}
+	}); 
+
+	Template.post_page.rendered = function(){
+		document.title = post.headline;
+		if((scrollToCommentId=Session.get('scrollToCommentId')) && !this.rendered && $('#'+scrollToCommentId).exists()){
+			scrollPageTo('#'+scrollToCommentId);
+			Session.set('scrollToCommentId', null);
+			this.rendered=true;
+		}
+	}
+}());
